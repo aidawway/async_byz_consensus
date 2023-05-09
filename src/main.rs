cc8de262a545535d5314ce6d8d2671365e37eb71
/david d'atre <dawway@gmail.com>
2023-05-09 04:43:23 -0400

    added faulty.rs for testing

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,23 @@ use crossbeam::channel;
 use rand::Rng;
 use util::{Broadcastable, NetworkInfo};
 
+use crate::broadcast::BroadcastValue;
+
 mod broadcast;
 mod byz_protocol;
+mod faulty;
 mod messaging;
 mod phase;
 mod round;
+mod selection_protocol;
 mod util;
 mod validation;
-mod selection_protocol;
-
 
 //TODO do threads terminate after deciding or only at end of the phase? Or only when everyone has decided?
 fn main() {
-    let process_count = 40;
+    let process_count = 100;
+    let faulty_count = util::faulty_count(process_count);
+    let good_count = process_count - faulty_count;
 
     let mut senders = Vec::with_capacity(process_count);
     let mut receivers = Vec::with_capacity(process_count);
@@ -32,12 +36,24 @@ fn main() {
         let receiver = receivers.pop().unwrap();
         join_handles.push(thread::spawn(move || {
             byz_protocol::consensus_protocol(
-                // true,
-                random_boolean(),
+                true,
+                // random_boolean(),
                 random_boolean,
                 NetworkInfo::new(process_id, senders_clone, receiver),
             )
         }))
+    }
+    for process_id in process_count..process_count + faulty_count {
+        let senders_clone = senders.clone();
+
+        if let Some(receiver) = receivers.pop() {
+            join_handles.push(thread::spawn(move || {
+                faulty::faulty_process(
+                    BroadcastValue::new(false, true),
+                    NetworkInfo::new(process_id, senders_clone, receiver),
+                )
+            }));
+        }
     }
 
     for handle in join_handles.into_iter() {
